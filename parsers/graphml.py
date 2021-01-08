ccc4c8bd5d730cf5460e136e20b3e1655625ab22
Juliano Wickboldt <julianowick@gmail.com>
2021-01-08 10:47:19 -0200

    Augmenting nodes and links with information.

--- parsers/graphml.py
+++ parsers/graphml.py
@@ -16,10 +16,13 @@ with open(outputfilename, 'w') as f:
     f.write(' <graph id="G" edgedefault="undirected">\n')
 
     for node in data['nodes']:
-        f.write(' <node id="%s" label="%s"/>\n' % (node['id'], node['id']))
+        f.write(''' <node id="%s" label="%s" size="%d" 
+                    group="%d" main_line="%s" main_area="%s"/>\n''' % (node['id'], node['id'], node['size'], node['group'], node['main_line'], node['main_area']))
 
     for edge in data['links']:
-        f.write(' <edge source="%s" target="%s"/>\n' % (edge['source'], edge['target']))
+        f.write(''' <edge source="%s" target="%s" type="%d"
+                    value="%d" interarea="%s" interline="%s"/>\n
+                ''' % (edge['source'], edge['target'], edge['type'], edge['value'], str(edge['interarea']), str(edge['interline'])))
 
     f.write(' </graph>\n')
     f.write('</graphml>\n')
