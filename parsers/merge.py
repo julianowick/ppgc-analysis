0832e8eda00b9d3b0b5cc1fac8d9e9638372ff32
Juliano Wickboldt <julianowick@gmail.com>
2021-01-12 17:47:17 -0200

    Adding script to merge CSV files (with identical headers).

---
+++ parsers/merge.py
@@ -0,0 +1,44 @@
+import csv
+
+basefilename = input('Which CSV file would you like to open (comma separated, utf-8): ')
+try:
+    with open(basefilename, newline='', encoding='utf8') as csvfile:
+        basefreader = csv.reader(csvfile, delimiter=',')
+        baseheaders = next(basefreader) # Read just the first line (headers)
+        # Find in the CSV file the rows maching all the key/value pairs
+        outfilename = basefilename[0:-4] + '-merged.csv'
+        outlines = 0
+        with open(outfilename, 'w', newline='', encoding='utf8') as csvoutfile:
+            fwriter = csv.writer(csvoutfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+            fwriter.writerow(baseheaders) # Write headers into the destination file
+            for row in basefreader:
+                fwriter.writerow(row)
+                outlines += 1
+
+except FileNotFoundError as e:
+    print('There was an issue processing the file:', str(e))
+
+onemorefile = 'y'
+files = 1
+while(onemorefile.lower() == 'y'):
+    otherfilename = input('Which CSV file would you like to merge (comma separated, utf-8): ')
+    try:
+        with open(otherfilename, newline='', encoding='utf8') as csvfile:
+            otherfreader = csv.reader(csvfile, delimiter=',')
+            otherheaders = next(otherfreader) # Read just the first line (headers)
+
+            if otherheaders == baseheaders: # can only merge identical CSV headers
+                with open(outfilename, 'a', newline='', encoding='utf8') as csvoutfile:
+                    fwriter = csv.writer(csvoutfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+                    for row in otherfreader:
+                        fwriter.writerow(row)
+                        outlines += 1
+
+        
+    except FileNotFoundError as e:
+        print('There was an issue processing the file:', str(e))
+
+    onemorefile = input('Do you need to include one more file to merge (y/n)? ')
+    files += 1
+
+print('Finished writing %d lines to "%s"' % (outlines, outfilename))
