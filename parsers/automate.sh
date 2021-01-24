cec21e6ad715f9f6b2805e204a25b393cc1d8078
Juliano Wickboldt <jwickboldt@pinkfloyd.inf.ufrgs.br>
2021-01-24 16:37:03 +0000

    Removing unnecessary files after parsing.

--- parsers/automate.sh
+++ parsers/automate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-CD_PROGRAMA_IES="42007011006P5"
-ENTIDADE_ENSINO="UNISINOS"
-PROGRAMA="PPGCA"
+CD_PROGRAMA_IES="31001017004P3"
+ENTIDADE_ENSINO="UFRJ"
+PROGRAMA="PESC-COPPE"
 
 /usr/bin/python3 parsers/filter.py data/producao-2017a2020-anais.csv CD_PROGRAMA_IES $CD_PROGRAMA_IES
 mv data/producao-2017a2020-anais-filtered.csv data/producao-2017a2020-anais-$CD_PROGRAMA_IES.csv 
@@ -21,4 +21,9 @@ mv data/producao-2017a2020-anais-$CD_PROGRAMA_IES-merged.csv data/producao-2017a
 /usr/bin/python3 parsers/merge.py data/prod-autor-2017a2020-anais-$CD_PROGRAMA_IES.csv data/prod-autor-2017a2020-artpe-$CD_PROGRAMA_IES.csv
 mv data/prod-autor-2017a2020-anais-$CD_PROGRAMA_IES-merged.csv data/prod-autor-2017a2020-$CD_PROGRAMA_IES.csv
 
-/usr/bin/python3 parsers/sucupira.py data/prod-autor-2017a2020-$CD_PROGRAMA_IES.csv data/producao-2017a2020-$CD_PROGRAMA_IES.csv data/graph-$PROGRAMA-$ENTIDADE_ENSINO-2017-2020.json
+rm data/producao-2017a2020-anais-$CD_PROGRAMA_IES.csv
+rm data/producao-2017a2020-artpe-$CD_PROGRAMA_IES.csv
+rm data/prod-autor-2017a2020-anais-$CD_PROGRAMA_IES.csv
+rm data/prod-autor-2017a2020-artpe-$CD_PROGRAMA_IES.csv
+
+/usr/bin/python3 parsers/sucupira.py data/prod-autor-2017a2020-$CD_PROGRAMA_IES.csv data/producao-2017a2020-$CD_PROGRAMA_IES.csv data/graph-$ENTIDADE_ENSINO-$PROGRAMA-2017-2020.json
